037671b16043eaca8169ff6011b30638bface4ce
bpreki <prescott.byron@gmail.com>
2019-03-08 21:52:28 +0000

    initial set up angular app

--- phonebook/.eslintrc.js
+++ phonebook/.eslintrc.js
@@ -3,14 +3,13 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "standard",
+    "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        "ecmaVersion": 2018
     },
     "rules": {
     }
